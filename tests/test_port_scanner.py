f94935370a8156cf926f76d8f3d91effee0c8151
Wolf <wolf@tgwolf.com>
2022-10-27 16:07:10 +0100

    Add more test coverage

--- tests/test_port_scanner.py
+++ tests/test_port_scanner.py
@@ -1,32 +1,37 @@
 """
 Docs
 """
-
+import modules.ports as PSports
 import modules.utils as PSutils
 
 
-def test_secure_random() -> None:
+def test_ports() -> None:
     """
     Docs
     """
     errors = []
     count = 0
 
-    v = PSutils.secure_random(1, 10)
-    if v not in range(1, 10):
-        errors.append(f"Test {count} security_random failed")
+    if PSports.get_ports_by_number('22') != 22:
+        errors.append(f"Test {count} get_ports_by_name'ssh'")
+
+    if PSports.get_ports_by_name('ssh') != 22:
+        errors.append(f"Test {count} get_ports_by_name'ssh'")
 
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))  # nosec: B101
 
 
-def test_shuffled() -> None:
+def test_utils() -> None:
     """
     Docs
     """
     errors = []
     count = 0
-
     test_list = [1, 2, 3, 4, 5]
+
+    v = PSutils.secure_random(1, 10)
+    if v not in range(1, 10):
+        errors.append(f"Test {count} security_random failed")
 
     v = PSutils.shuffled(test_list)
     if v == test_list:
