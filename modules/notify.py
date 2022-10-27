65e274a75949b94458046364976a65b6766f489e
Wolf <wolf@tgwolf.com>
2022-10-27 18:36:56 +0100

    more tests and cleanup of tests

--- modules/notify.py
+++ modules/notify.py
@@ -12,11 +12,25 @@ def success(message: str) -> None:
     print(f"{PSconstants.SUCCESS}{message}{PSconstants.RESET}")
 
 
+def success_msg(message: str) -> str:
+    """
+    Docs
+    """
+    return f"{PSconstants.SUCCESS}{message}{PSconstants.RESET}"
+
+
 def warn(message: str) -> None:
     """
     Docs
     """
     print(f"{PSconstants.WARN}{message}{PSconstants.RESET}")
+
+
+def warn_msg(message: str) -> str:
+    """
+    Docs
+    """
+    return f"{PSconstants.WARN}{message}{PSconstants.RESET}"
 
 
 def error(message: str) -> None:
@@ -26,8 +40,22 @@ def error(message: str) -> None:
     print(f"{PSconstants.ERROR}{message}{PSconstants.RESET}")
 
 
+def error_msg(message: str) -> str:
+    """
+    Docs
+    """
+    return f"{PSconstants.ERROR}{message}{PSconstants.RESET}"
+
+
 def info(message: str) -> None:
     """
     Docs
     """
     print(f"{PSconstants.INFO}{message}{PSconstants.RESET}")
+
+
+def info_msg(message: str) -> str:
+    """
+    Docs
+    """
+    return f"{PSconstants.INFO}{message}{PSconstants.RESET}"
--- tests/test_port_scanner.py
+++ tests/test_port_scanner.py
@@ -12,32 +12,34 @@ def test_notify(capfd) -> None:
     """
     test notify
     """
+    tests = [
+              {'function': PSnotify.success, 'color': PSconstants.SUCCESS, 'strip': True, 'return_value': False},
+              {'function': PSnotify.warn, 'color': PSconstants.WARN, 'strip': True, 'return_value': False},
+              {'function': PSnotify.error, 'color': PSconstants.ERROR, 'strip': True, 'return_value': False},
+              {'function': PSnotify.info, 'color': PSconstants.INFO, 'strip': True, 'return_value': False},
+              {'function': PSnotify.success_msg, 'color': PSconstants.SUCCESS, 'strip': False, 'return_value': True},
+              {'function': PSnotify.warn_msg, 'color': PSconstants.WARN, 'strip': False, 'return_value': True},
+              {'function': PSnotify.error_msg, 'color': PSconstants.ERROR, 'strip': False, 'return_value': True},
+              {'function': PSnotify.info_msg, 'color': PSconstants.INFO, 'strip': False, 'return_value': True},
+    ]
     errors = []
     count = 0
+    clean_string = "hello world"
 
-    test_string = f"{PSconstants.SUCCESS}test{PSconstants.RESET}"
-    PSnotify.success("test")
-    out, err = capfd.readouterr()
-    if test_string != out.strip():
-        errors.append(f"Test success failed '{test_string}' vs '{out}'")
+    for test in tests:
+        count += 1
+        test_string = f"{test['color']}{clean_string}{PSconstants.RESET}"
 
-    test_string = f"{PSconstants.WARN}test{PSconstants.RESET}"
-    PSnotify.warn("test")
-    out, err = capfd.readouterr()
-    if test_string != out.strip():
-        errors.append(f"Test warn failed '{test_string}' vs '{out}'")
+        if test['return_value'] is True:
+            result_string = test['function'](clean_string)  # pylint: disable=assignment-from-no-return
+        else:
+            test['function'](clean_string)
+            result_string, _err = capfd.readouterr()
 
-    test_string = f"{PSconstants.ERROR}test{PSconstants.RESET}"
-    PSnotify.error("test")
-    out, err = capfd.readouterr()
-    if test_string != out.strip():
-        errors.append(f"Test error failed '{test_string}' vs '{out}'")
-
-    test_string = f"{PSconstants.INFO}test{PSconstants.RESET}"
-    PSnotify.info("test")
-    out, err = capfd.readouterr()
-    if test_string != out.strip():
-        errors.append(f"Test info failed '{test_string}' vs '{out}'")
+        if test['strip'] is True:
+            result_string = result_string.strip()
+        if test_string != result_string:
+            errors.append(f"Test {count} failed: {test_string} vs {result_string}")
 
     assert not errors, "errors occurred:\n{}".format("\n".join(errors))  # nosec: B101
 
@@ -50,10 +52,12 @@ def test_ports() -> None:
     count = 0
 
     port = PSports.get_ports_by_number('22')
+    count += 1
     if port != [22]:
         errors.append(f"Test {count} get_ports_by_name'ssh' failed: {port}")
 
     port = PSports.get_ports_by_name('ssh')
+    count += 1
     if port != [22]:
         errors.append(f"Test {count} get_ports_by_name'ssh' failed: {port}")
 
@@ -69,10 +73,12 @@ def test_utils() -> None:
     test_list = [1, 2, 3, 4, 5]
 
     rint = PSutils.secure_random(1, 10)
+    count += 1
     if rint not in range(1, 11):
         errors.append(f"Test {count} security_random failed: {rint}")
 
     slist = PSutils.shuffled(test_list)
+    count += 1
     if slist == test_list:
         errors.append(f"Test {count} shuffled failed failed: {slist}")
 
