60a3856555a97856a6388c4aa64efa8009937681
Wolf <wolf@tgwolf.com>
2022-10-27 15:53:39 +0100

    get code coverage working

--- modules/notify.py
+++ modules/notify.py
@@ -7,23 +7,27 @@ import modules.constants as PSconstants
 
 def success(message: str) -> None:
     """
+    Docs
     """
     print(f"{PSconstants.SUCCESS}{message}{PSconstants.RESET}")
 
 
 def warn(message: str) -> None:
     """
+    Docs
     """
     print(f"{PSconstants.WARN}{message}{PSconstants.RESET}")
 
 
 def error(message: str) -> None:
     """
+    Docs
     """
     print(f"{PSconstants.ERROR}{message}{PSconstants.RESET}")
 
 
 def info(message: str) -> None:
     """
+    Docs
     """
     print(f"{PSconstants.INFO}{message}{PSconstants.RESET}")
---
+++ tests/test_port_scanner.py
@@ -0,0 +1,35 @@
+"""
+Docs
+"""
+
+import modules.utils as PSutils
+
+
+def test_secure_random() -> None:
+    """
+    Docs
+    """
+    errors = []
+    count = 0
+
+    v = PSutils.secure_random(1, 10)
+    if v not in range(1, 10):
+        errors.append(f"Test {count} security_random failed")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))  # nosec: B101
+
+
+def test_shuffled() -> None:
+    """
+    Docs
+    """
+    errors = []
+    count = 0
+
+    test_list = [1,2,3,4,5]
+
+    v = PSutils.shuffled(test_list)
+    if v == test_list:
+        errors.append(f"Test {count} shuffled failed failed")
+
+    assert not errors, "errors occurred:\n{}".format("\n".join(errors))  # nosec: B101
